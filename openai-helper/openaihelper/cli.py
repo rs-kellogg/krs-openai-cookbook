ededdc24f6781d6d0330550ef7d72895596dbca8
William Thompson <will.k.t@gmail.com>
2024-08-11 19:25:59 -0500

    updates

--- openai-helper/openaihelper/cli.py
+++ openai-helper/openaihelper/cli.py
@@ -1,11 +1,12 @@
 import os
 import csv
 import json
-import yaml
+import math
 
 import fitz
 import typer
 import openai
+import chevron
 import logging
 import logging.config
 
@@ -16,9 +17,11 @@ from tqdm import tqdm
 from pathlib import Path
 from typing import Optional
 from dotenv import load_dotenv
-from rich import console as cons
+from rich.table import Table
+from rich.console import Console
 from importlib import resources
 from tenacity import wait_random_exponential
+from typing_extensions import Annotated
 
 from openaihelper import functions as F
 from openaihelper import data
@@ -33,18 +36,14 @@ load_dotenv()
 
 # load in the configuration file
 with resources.path(data, "config.yml") as path:
-    CONFIG_FILE_PATH = Path(path)
-    CONFIG_FILE_PATH.touch(exist_ok=True)
-
-with open(CONFIG_FILE_PATH) as conf_file:
-    CONFIG = yaml.load(conf_file, Loader=yaml.FullLoader)
+    CONFIG = F.config(path)
 
 # setup logging
 logging.config.dictConfig(CONFIG["logging"])
 logger = logging.getLogger(__name__)
 
 # setup the rich console
-console = cons.Console(style="green on black")
+console = Console(style="green on black")
 
 # setup the typer app
 app = typer.Typer()
@@ -60,39 +59,27 @@ client = openai.OpenAI(
 # -----------------------------------------------------------------------------
 # helper functions
 # -----------------------------------------------------------------------------
-def _version_callback(value: bool) -> None:
+def version_callback(value: bool) -> None:
     if value:
         console.print(f"[blue]version: {__app_name__} v{__version__}")
         raise typer.Exit()
 
 
-def check_args(
-    data_file_path: Path,
-    config_file_path: Path,
-    outdir: Path,
-):
-    assert data_file_path.exists()
-    assert config_file_path.exists()
-    if not outdir.exists():
-        outdir.mkdir(parents=True)
-
-
 # -----------------------------------------------------------------------------
 # commands
 # -----------------------------------------------------------------------------
-
-
-# -----------------------------------------------------------------------------
 @app.callback()
 def main(
-    version: Optional[bool] = typer.Option(
-        None,
-        "--version",
-        "-v",
-        help="Show the application's version and exit.",
-        callback=_version_callback,
-        is_eager=True,
-    )
+    version: Annotated[
+        Optional[bool],
+        typer.Option(
+            "--version",
+            "-v",
+            help="Show the application's version and exit.",
+            callback=version_callback,
+            is_eager=True,
+        ),
+    ] = None
 ) -> Optional[bool]:
     return version
 
@@ -100,26 +87,22 @@ def main(
 # -----------------------------------------------------------------------------
 @app.command()
 def config() -> None:
-    console.print(f"[orange1]{CONFIG_FILE_PATH}")
-    console.print(f"[green]{CONFIG}")
+    console.print(f"{CONFIG}")
 
 
 # -----------------------------------------------------------------------------
 @app.command()
 def pdf2text(
-    in_dir: Path = typer.Argument(..., help="Path to input PDF files"),
-    out: Optional[Path] = typer.Option(
-        Path("."),
-        "--dir",
-        help="The directory where the extracted output files will be created.",
-    ),
-    start: Optional[int] = typer.Option(0, help="Start page"),
-    end: Optional[int] = typer.Option(10_000, help="End page"),
+    in_dir: Annotated[Path, typer.Argument(help="Path to input PDF files")] = None,
+    out: Annotated[Path, typer.Option(help="Path to output text files")] = Path("."),
+    start: Annotated[int, typer.Option(help="Start page")] = 0,
+    end: Annotated[int, typer.Option(help="End page")] = math.inf,
 ):
     """
     Extract text from a collection of PDF files and write each output to a text file.
     """
     assert end >= start
+    check_args(in_dir, CONFIG_FILE_PATH, out)
     if not out.exists():
         out.mkdir(parents=True)
 
@@ -138,102 +121,74 @@ def pdf2text(
 
 # -----------------------------------------------------------------------------
 @app.command()
-def count_tokens(
-    data_file_path: Path = typer.Argument(..., help="Data file path name"),
-    config_file_path: Path = typer.Argument(..., help="Config file path name"),
-    out: Path = typer.Option(Path("."), help="Output directory"),
-):
-    """
-    Count tokens in a data file and write output to a csv file.
-    """
-    check_args(data_file_path, config_file_path, out)
-
-    # Read config file
-    config = F.config(config_file_path)
-    encoding_name = config["encoding_name"]
-    max_token_len = config["max_token_len"]
-
-    # Read data file
-    df = pd.read_csv(data_file_path)
-    assert "id" in df.columns
-    assert "text" in df.columns
-    texts = list(df["text"])
-    ids = list(df["id"])
-
-    # Count tokens and write results to csv file
-    out_csv = open(f"{out}/{data_file_path.stem}_counts.csv", "a")
-    writer = csv.writer(out_csv)
-    writer.writerow(["id", "count"])
-    for i in tqdm(range(len(texts))):
-        n_tokens = F.count_tokens(texts[i], encoding_name)
-        writer.writerow([ids[i], n_tokens])
-        out_csv.flush()
-        logging.info(f"Data point {ids[i]} has {n_tokens} tokens")
-    out_csv.close()
-
-
-# -----------------------------------------------------------------------------
-@app.command()
-def complete_prompt(
-    data_file_path: Path = typer.Argument(..., help="Data file path name"),
-    config_file_path: Path = typer.Argument(..., help="Config file path name"),
-    out: Path = typer.Option(Path("."), help="Output directory"),
+def chat_complete(
+    data_file: Annotated[Path, typer.Argument(help="Data file")] = None,
+    config_file: Annotated[Path, typer.Argument(help="Config file")] = None,
+    model: Annotated[str, typer.Option(help="Model name")] = "gpt-4o",
+    out: Annotated[Path, typer.Option(help="Path to output text files")] = Path("."),
+    count: Annotated[bool, typer.Option(help="Count input tokens")] = False,
 ):
     """
     Accept a data file with text and complete the prompt for each text and write output to a csv file.
     """
-    check_args(data_file_path, config_file_path, out)
+    assert data_file.exists()
+    assert config_file.exists()
+    if not out.exists():
+        out.mkdir(parents=True)
+    
+    config = F.config(config_file)
 
-    # Read config file and setup OpenAI
-    config = F.config(config_file_path)
-    encoding_name = config["encoding_name"]
-    max_token_len = config["max_token_len"]
-    model_name = config["model_name"]
-    user_prompt = config["user_prompt"]
-    system_prompt = config["system_prompt"]
-    n_prompt_tokens = F.count_tokens(system_prompt + user_prompt, encoding_name)
+    typer.Exit()
 
-    # Read the data file and check its format
-    df = pd.read_csv(data_file_path)
-    assert "id" in df.columns
-    assert "text" in df.columns
-    texts = list(df["text"])
-    ids = list(df["id"])
+    # config = F.config(config_file_path)
+    # encoding_name = config["encoding_name"]
+    # max_token_len = config["max_token_len"]
+    # model_name = config["model_name"]
+    # user_prompt = config["user_prompt"]
+    # system_prompt = config["system_prompt"]
+    # n_prompt_tokens = F.count_tokens(system_prompt + user_prompt, encoding_name)
 
-    # Complete prompt for each row and write results to csv file
-    out_csv_path = out / f"{data_file_path.stem}_responses.csv"
-    if not out_csv_path.exists():
-        out_csv = open(out_csv_path, "w")
-        writer = csv.writer(out_csv)
-        writer.writerow(["id", "response"])
-        out_csv.close()
+    # # Read the data file and check its format
+    # df = pd.read_csv(data_file_path)
+    # assert "id" in df.columns
+    # assert "text" in df.columns
+    # texts = list(df["text"])
+    # ids = list(df["id"])
 
-    with open(f"{out}/{data_file_path.stem}_responses.csv", "a") as out_csv:
-        writer = csv.writer(out_csv)
+    # # Complete prompt for each row and write results to csv file
+    # out_csv_path = out / f"{data_file_path.stem}_responses.csv"
+    # if not out_csv_path.exists():
+    #     out_csv = open(out_csv_path, "w")
+    #     writer = csv.writer(out_csv)
+    #     writer.writerow(["id", "response"])
+    #     out_csv.close()
 
-        for i in tqdm(range(len(texts))):
-            # check if the text is too long
-            n_tokens = F.count_tokens(texts[i], encoding_name)
-            if (n_tokens + n_prompt_tokens) > max_token_len:
-                writer.writerow([ids[i], "TOO_LONG"])
-                logging.warn(f"Data point {ids[i]} not completed")
-            else:
-                # complete the prompt
-                response = F.chat_complete(
-                    client=client,
-                    model_name=model_name,
-                    user_prompt=user_prompt,
-                    system_prompt=system_prompt,
-                    text=texts[i],
-                )
-                # validate the response
-                valid = F.validate_result(response)
-                if valid:
-                    logging.info(f"Data point {ids[i]} completed")
-                    writer.writerow([ids[i], json.dumps(response)])
-                else:
-                    logging.warn(f"Error for data point {ids[i]}: {response}")
-            out_csv.flush()
+    # with open(f"{out}/{data_file_path.stem}_responses.csv", "a") as out_csv:
+    #     writer = csv.writer(out_csv)
+
+    #     for i in tqdm(range(len(texts))):
+    #         # check if the text is too long
+    #         n_tokens = F.count_tokens(texts[i], encoding_name)
+    #         if (n_tokens + n_prompt_tokens) > max_token_len:
+    #             writer.writerow([ids[i], "TOO_LONG"])
+    #             logging.warn(f"Data point {ids[i]} not completed")
+    #         else:
+    #             # complete the prompt
+    #             response = F.chat_complete(
+    #                 client=client,
+    #                 model_name=model_name,
+    #                 user_prompt=user_prompt,
+    #                 system_prompt=system_prompt,
+    #                 text=texts[i],
+    #             )
+    #             # validate the response
+    #             valid = F.validate_result(response)
+    #             if valid:
+    #                 logging.info(f"Data point {ids[i]} completed")
+    #                 writer.writerow([ids[i], json.dumps(response)])
+    #             else:
+    #                 logging.warn(f"Error for data point {ids[i]}: {response}")
+    #         out_csv.flush()
 
 
 # -----------------------------------------------------------------------------
